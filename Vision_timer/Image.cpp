e349e7b044c41df626989797e50dba2cf8d54580
Kevin Bosman <kevin_bosman_2@hotmail.com>
2014-03-05 12:05:06 +0100

    Image Class Kevin Sample

---
+++ Vision_timer/Image.cpp
@@ -0,0 +1,105 @@
+#include "Image.h"
+
+/*
+Constructor
+*/
+Image::Image() {
+	image = new CImage();
+	width = 0;
+	height = 0;
+	bpp = 0;
+}
+
+Image::Image(std::string inputName) {
+	image = new CImage();
+	fileName = inputName;
+	image->Load(_T((inputLocation + fileName).c_str()));
+	LoadFromCImage();
+}
+
+Image::Image(Image &img) {
+	width = img.width;
+	height = img.height;
+	bpp = img.bpp;
+	fileName = img.fileName;
+	pixel = new unsigned int[height * width];
+	for(int i = 0; i < (width * height); i++) {
+		pixel[i] = img.pixel[i];
+	}
+	image = new CImage();
+	image->Create(width, height, bpp);
+}
+
+Image::~Image() {
+	delete image;
+	delete[] pixel;
+}
+
+
+
+
+
+/*
+Public functions
+*/
+
+void Image::LoadImageFromFile(std::string inputName) {
+	fileName = inputName;
+	image->Load(_T((inputLocation + fileName).c_str()));
+	LoadFromCImage();
+}
+
+void Image::SaveImageToFile(std::string prefixName) {
+	int R = 0, G = 0, B = 0, A = 0, currentPixel;
+
+	//Pitch (negative = Bottom-up DIB / positive = Bottom-down DIB)
+	//locating pointer to start of image data
+	int pitch = image->GetPitch();
+	BYTE* imgPtr = (BYTE*)image->GetBits();
+
+	for(int y = image->GetHeight() - 1; y >= 0; y--) {
+		for(int x = image->GetWidth() - 1; x >= 0; x--) {
+			currentPixel = pixel[x + (y * width)];
+			*(imgPtr + pitch * y + 3 * x) = (currentPixel >> bluePixelShift) & 0xFF;
+			*(imgPtr + pitch * y + 3 * x + 1) = (currentPixel >> greenPixelShift) & 0xFF;
+			*(imgPtr + pitch * y + 3 * x + 2) = (currentPixel >> redPixelShift) & 0xFF;
+			if(bpp > 24) {
+				*(imgPtr + pitch * y + 3 * x + 3) = (currentPixel & 0xFF);
+			}
+		}
+	}
+
+	std::cout << (outputLocation + prefixName + fileName).c_str() << std::endl;
+	image->Save(_T((outputLocation + prefixName + fileName).c_str()));
+}
+
+
+
+/*
+Private functions
+Array pixel[x + (y * width)]
+*/
+void Image::LoadFromCImage() {
+	height = image->GetHeight();
+	width = image->GetWidth();
+	bpp = image->GetBPP();
+	pixel = new unsigned int[height * width];
+	int R = 0, G = 0, B = 0, A = 0;
+
+	//Pitch (negative = Bottom-up DIB / positive = Bottom-down DIB)
+	//locating pointer to start of image data
+	int pitch = image->GetPitch();
+	BYTE* imgPtr = (BYTE*)image->GetBits();
+
+	for(int y = image->GetHeight() - 1; y >= 0; y--) {
+		for(int x = image->GetWidth() - 1; x >= 0; x--) {
+			B = *(imgPtr + pitch * y + 3 * x);
+			G = *(imgPtr + pitch * y + 3 * x + 1);
+			R = *(imgPtr + pitch * y + 3 * x + 2);
+			if(bpp > 24) {
+				A = *(imgPtr + pitch * y + 3 * x + 3);
+			}
+			pixel[x + (y * width)] = (R << redPixelShift) | (G << greenPixelShift) | (B << bluePixelShift) | A;
+		}
+	}
+}
