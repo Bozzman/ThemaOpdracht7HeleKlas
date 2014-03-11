1710ebbbfb348b5229fe77afe4f419ed60823c9e
Kevin Bosman <kevin_bosman_2@hotmail.com>
2014-03-11 17:01:20 +0100

    Update image

--- Vision_timer/Image.cpp
+++ Vision_timer/Image.cpp
@@ -8,6 +8,20 @@ Image::Image() {
 	width = 0;
 	height = 0;
 	bpp = 0;
+	pixel = nullptr;
+}
+
+/*
+Create an empty image
+*/
+Image::Image(std::string fileName, int width, int height) : width(width), height(height), fileName(fileName) {
+	image = new CImage();
+	image->Create(width, height, 24);
+	bpp = 24;
+	pixel = new unsigned int[height * width];
+	for(int i = 0; i < (width * height); i++) {
+		pixel[i] = 0;
+	}
 }
 
 /*
@@ -52,6 +66,23 @@ Image::~Image() {
 Public functions
 */
 
+/*
+Copy an image
+*/
+void Image::CopyImage(Image &img) {
+	width = img.width;
+	height = img.height;
+	bpp = img.bpp;
+	fileName = img.fileName;
+	delete[] pixel;
+	pixel = new unsigned int[height * width];
+	for(int i = 0; i < (width * height); i++) {
+		pixel[i] = img.pixel[i];
+	}
+	delete image;
+	image = new CImage();
+	image->Create(width, height, bpp);
+}
 
 /*
 Load a file
@@ -66,30 +97,30 @@ void Image::LoadImageFromFile(std::string inputName) {
 Save an image to a file
 */
 void Image::SaveImageToFile(std::string prefixName) {
-	int R = 0, G = 0, B = 0, A = 0, currentPixel;
+	if(pixel != nullptr) {
+		int R = 0, G = 0, B = 0, A = 0, currentPixel;
 
-	//Pitch (negative = Bottom-up DIB / positive = Bottom-down DIB)
-	//locating pointer to start of image data
-	int pitch = image->GetPitch();
-	BYTE* imgPtr = (BYTE*)image->GetBits();
+		//Pitch (negative = Bottom-up DIB / positive = Bottom-down DIB)
+		//locating pointer to start of image data
+		int pitch = image->GetPitch();
+		BYTE* imgPtr = (BYTE*)image->GetBits();
 
-	for(int y = image->GetHeight() - 1; y >= 0; y--) {
-		for(int x = image->GetWidth() - 1; x >= 0; x--) {
-			currentPixel = pixel[x + (y * width)];
-			*(imgPtr + pitch * y + 3 * x) = (currentPixel >> bluePixelShift) & 0xFF;
-			*(imgPtr + pitch * y + 3 * x + 1) = (currentPixel >> greenPixelShift) & 0xFF;
-			*(imgPtr + pitch * y + 3 * x + 2) = (currentPixel >> redPixelShift) & 0xFF;
-			if(bpp > 24) {
-				*(imgPtr + pitch * y + 3 * x + 3) = (currentPixel & 0xFF);
+		for(int y = image->GetHeight() - 1; y >= 0; y--) {
+			for(int x = image->GetWidth() - 1; x >= 0; x--) {
+				currentPixel = pixel[x + (y * width)];
+				*(imgPtr + pitch * y + 3 * x) = (currentPixel >> bluePixelShift) & 0xFF;
+				*(imgPtr + pitch * y + 3 * x + 1) = (currentPixel >> greenPixelShift) & 0xFF;
+				*(imgPtr + pitch * y + 3 * x + 2) = (currentPixel >> redPixelShift) & 0xFF;
+				if(bpp > 24) {
+					*(imgPtr + pitch * y + 3 * x + 3) = (currentPixel & 0xFF);
+				}
 			}
 		}
+
+		std::cout << (outputLocation + prefixName + fileName).c_str() << std::endl;
+		image->Save(_T((outputLocation + prefixName + fileName).c_str()));
 	}
-
-	std::cout << (outputLocation + prefixName + fileName).c_str() << std::endl;
-	image->Save(_T((outputLocation + prefixName + fileName).c_str()));
 }
-
-
 
 /*
 Private functions
